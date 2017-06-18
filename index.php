1d6241d544eb7ce2b141c39e059bd207c194276d
Djenova <agrid@outlook.com>
2017-06-19 02:36:52 +0700

    finish to show domain with criteria 1. Last 12 onthisday 2. Avaiable .com .org .info .co .us

--- index.php
+++ index.php
@@ -1,8 +1,18 @@
 <?php
+date_default_timezone_set("UTC");
+$date = date("Y-m-d");
 $ch = curl_init();
 $postData = array(
     'login' => 'ahgryd',
     'password' => 'jadilahlegenda'
+);
+$postData2 = array(
+    'fadddate' => '$date',
+    'fstatusorgfree' => '22',
+    'fstatuscomfree' => '22',
+    'fstatususfree' => '22',
+    'fstatusinfofree' =>'22',
+    'fstatususfree' => '22'
 );
 
 curl_setopt_array($ch, array(
@@ -19,10 +29,16 @@ curl_setopt_array($ch, array(
     CURLOPT_SSL_VERIFYHOST =>false,
     //CURLOPT_URL => 'https://member.expireddomains.net/domains/expiredcom/'
 ));
-
-
 $output = curl_exec($ch);
-curl_setopt($ch, CURLOPT_URL, 'https://member.expireddomains.net/domains/expiredcom/');
+curl_setopt_array($ch, array(
+    CURLOPT_URL => 'https://member.expireddomains.net/export/expiredcom/?export=textfile&&flast12=1flimit=25&fstatuscomfree=22&fstatusorgfree=22&fstatusinfofree=22&fadddate='.$date.'&fstatususfree=22&fstatuscofree=22',
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_FOLLOWLOCATION => true,
+    CURLOPT_USERAGENT => 'Mozilla/5.0 (Windows; U; Windows NT 5.0; en-US; rv:1.7.12) Gecko/20050915 Firefox/1.0.7',
+));
+
+
+//curl_setopt($ch, CURLOPT_URL, 'https://member.expireddomains.net/domains/expiredcom/');
 $content = curl_exec($ch);
 echo $content;
 //echo $output;
